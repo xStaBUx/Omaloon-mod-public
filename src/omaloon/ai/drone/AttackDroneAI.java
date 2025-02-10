40e285bec660de59ece82f6083eace69aa3e21d0
Zelaux <58040045+zelaux@users.noreply.github.com>
2025-02-11 00:35:41 +0300

    DroneAI better attacker movement

--- src/omaloon/ai/drone/AttackDroneAI.java
+++ src/omaloon/ai/drone/AttackDroneAI.java
@@ -1,47 +1,78 @@
 package omaloon.ai.drone;
 
+import arc.graphics.g2d.*;
+import arc.util.*;
 import mindustry.gen.*;
+import mindustry.graphics.*;
 import omaloon.ai.*;
+import omaloon.utils.*;
 
-public class AttackDroneAI extends DroneAI {
-    public AttackDroneAI(Unit owner) {
+public class AttackDroneAI extends DroneAI{
+
+    public static final float SMOOTH = 30f;
+
+    public AttackDroneAI(Unit owner){
         super(owner);
     }
 
     @Override
-    public void updateMovement() {
-        if (!owner.isShooting()) {
+    public void updateMovement(){
+        float clearOwnerRange = owner.type.range;
+        float realRange = clearOwnerRange + unit.type.range;
+        if(!unit.hasWeapons()) return;
+        if(!isOwnerShooting()){
             rally();
             return;
         }
-        if (!unit.hasWeapons()) return;
+        if(DebugDraw.isDraw()){
+            DebugDraw.request(Layer.end, () -> {
+                Draw.color(Pal.negativeStat);
+                Lines.circle(owner.x, owner.y, clearOwnerRange);
+                Draw.color(Pal.health);
+                Lines.circle(owner.x, owner.y, realRange);
+
+                Drawf.target(posTeam.x, posTeam.y, 6f, Pal.accent);
+
+                Draw.color(Pal.removeBack);
+                Lines.circle(unit.x, unit.y, unit.type.range);
+            });
+        }
 
         posTeam.set(owner.aimX(), owner.aimY());
-
-        float distanceToTarget = unit.dst(posTeam);
-        float distanceToOwner = unit.dst(owner);
+        float ownerToPosDst2 = owner.dst2(posTeam);
 
         unit.lookAt(posTeam);
 
-        if (distanceToOwner < owner.range()) {
-            moveTo(posTeam, unit.type().range * 0.75f, 30f);
-            return;
-        }
 
-        moveTo(owner, owner.range() * 0.95f, 30f);
-        if (distanceToTarget > unit.type().range) {
-            unit.lookAt(posTeam);
-            unit.controlWeapons(true, true);
+        float range = unit.type().range;
+        float ownerDst2 = owner.dst2(unit);
+
+        if(ownerDst2<clearOwnerRange*clearOwnerRange){
+            moveTo(posTeam, range * 0.75f, SMOOTH);
+        }else{
+            Tmp.v1.set(unit).sub(owner);
+            Tmp.v2.set(posTeam).sub(unit);
+            var extra=(clearOwnerRange+8);
+            if(Tmp.v1.dot(Tmp.v2) < 0 && ownerDst2<extra*extra){
+                moveTo(posTeam, range * 0.75f, SMOOTH);
+                //TODO change to angle check?
+            }else
+                moveTo(owner, clearOwnerRange, SMOOTH);
         }
+        unit.controlWeapons(true, true);
+    }
+
+    private boolean isOwnerShooting(){
+        return owner.isShooting() || owner.controller() instanceof Player player && player.shooting;
     }
 
     @Override
-    public Teamc target(float x, float y, float range, boolean air, boolean ground) {
-        return (!owner.isValid() && !owner.isShooting()) ? null : posTeam;
+    public Teamc target(float x, float y, float range, boolean air, boolean ground){
+        return (!owner.isValid() && !isOwnerShooting()) ? null : posTeam;
     }
 
     @Override
-    public boolean shouldShoot() {
-        return owner.isShooting();
+    public boolean shouldShoot(){
+        return isOwnerShooting();
     }
 }
